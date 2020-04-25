0a1530b92a69b94468107656ed0e9e95581099ad
duybinh98 <43360513+duybinh98@users.noreply.github.com>
2020-04-26 02:05:43 +0700

    Revert "small fix"
    This reverts commit a5dfda07bc18087a6679f2180cb62f051a73600c.

--- components/Requests/RequestListPendingItem.js
+++ components/Requests/RequestListPendingItem.js
@@ -39,7 +39,6 @@ export default class RequestListPendingItem extends Component {
                                 status: this.props.req_status,
                                 testsList: this.props.testList,
                                 totalAmount: this.props.req_amount,
-                                nurseName: this.props.nurse_name,
                                 // customerInfo  = this.state.customerInfo,
                             },
                         })
